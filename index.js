f548017a1b4536868b240969ded33f06aeaa823b
Amran Hossain <amran.h.akash@gmail.com>
2025-03-07 22:41:49 +0600

    cart poster api

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
 
         const menuCollection = client.db("myRestaurantDb").collection("menu")
         const reviewsCollection = client.db("myRestaurantDb").collection("reviews")
+        const cartCollection = client.db("myRestaurantDb").collection("carts")
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
@@ -39,6 +40,14 @@ async function run() {
             const result = await reviewsCollection.find().toArray();
             res.send(result)
         })
+
+        // Carts collection
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
